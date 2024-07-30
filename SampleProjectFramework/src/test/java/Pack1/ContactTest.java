68c1fe376c816cf555350d37054edc5c8338c3ab
Divya H M <divyahm01@gmail.com>
2024-07-30 17:29:15 +0530

    Committing to local Repository

---
+++ SampleProjectFramework/src/test/java/Pack1/ContactTest.java
@@ -0,0 +1,28 @@
+package Pack1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest
+{
+	@Test
+	public void createContactTest()
+	{
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		System.out.println("Execute create contact Test");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+	}
+	
+	@Test
+	public void modifyContactTest()
+	{
+		System.out.println("Execute modify contact Test");
+	}
+
+}
